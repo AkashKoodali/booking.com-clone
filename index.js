1c916ce0db22dab0d10c465cb669b9331e37a070
Akash Koodali <akashkoodali9@gmail.com>
2022-09-09 18:57:37 +0530

    set routes and middlewares

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import DbConnection from "./config/databaseConnection.js";
+
+import authRouter from "./routes/auth.js";
+import hotelsRouter from './routes/hotels.js';
+import roomsRouter from './routes/rooms.js';
+import usersRouter from "./routes/users.js";
 
 dotenv.config();
 
@@ -11,6 +17,15 @@ DbConnection();
 const PORT = 8800;
 
 app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("hello")
+});
+
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
+app.use("/rooms", roomsRouter);
+app.use("/hotels", hotelsRouter);
 
 app.listen( PORT, ()=> {
     console.log('Connected to backent');
